734cc95f5e80a42984e03e1dae8e38ee92e3eee1
Keith Horwood <keithwhor@gmail.com>
2017-07-15 17:18:14 -0700

    Upgrade to 3.0.0 for FaaSlang

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lib',
   packages = ['lib'],
-  version = '2.0.0',
+  version = '3.0.0',
   description = 'StdLib: Standard Library for Microservices Python Bindings',
   author = 'Keith Horwood',
   author_email = 'keithwhor@gmail.com',
   url = 'https://github.com/stdlib/lib-python',
-  keywords = ['stdlib', 'microservice', 'serverless'],
+  keywords = ['stdlib', 'microservice', 'serverless', 'faas', 'lib'],
   license='MIT',
   classifiers = [
     'Programming Language :: Python :: 2',
