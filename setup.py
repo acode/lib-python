5f89904e6cea5ddb0c18f2386965120dd8d5af80
Keith Horwood <keithwhor@gmail.com>
2021-12-13 18:18:42 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lib',
   packages = ['lib'],
-  version = '3.0.0',
-  description = 'StdLib: Standard Library for Microservices Python Bindings',
+  version = '4.0.0',
+  description = 'Autocode standard library Python bindings',
   author = 'Keith Horwood',
   author_email = 'keithwhor@gmail.com',
-  url = 'https://github.com/stdlib/lib-python',
-  keywords = ['stdlib', 'microservice', 'serverless', 'faas', 'lib'],
+  url = 'https://github.com/acode/lib-python',
+  keywords = ['autocode', 'stdlib', 'microservice', 'serverless', 'faas', 'lib'],
   license='MIT',
   classifiers = [
     'Programming Language :: Python :: 2',
